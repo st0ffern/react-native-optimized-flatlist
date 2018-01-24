502b09634ff0853abbf971375f969a205f644e80
Josh Sturrock <hello@jxsh.co>
2018-01-24 09:01:58 +0000

    feat(Scroll Methods): Added FlatList scroll methods (#25)
    fixes #4

--- src/OptimizedFlatList.js
+++ src/OptimizedFlatList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { 
+import {
   FlatList,
 } from 'react-native'
 import FlatListItem from './FlatListItem'
@@ -11,6 +11,10 @@ export default class OptimizedFlatList extends React.PureComponent {
     this.state = {}
     this.rowRefs =[]
     this._onViewableItemsChanged = this._onViewableItemsChanged.bind(this)
+    this.scrollToEnd = this.scrollToEnd.bind(this)
+    this.scrollToIndex = this.scrollToIndex.bind(this)
+    this.scrollToItem = this.scrollToItem.bind(this)
+    this.scrollToOffset = this.scrollToOffset.bind(this)
   }
 
   _addRowRefs(ref, data){
@@ -20,7 +24,7 @@ export default class OptimizedFlatList extends React.PureComponent {
       index: data.index,
     }
   }
-  
+
   _updateItem(index, visibility){
         if (!this.rowRefs[index].ref) {
       return false;
@@ -50,14 +54,49 @@ export default class OptimizedFlatList extends React.PureComponent {
       }>
     }
   ) {
-    info.changed.map(item => 
+    info.changed.map(item =>
       this._updateItem(item.index, item.isViewable)
     )
+  }
+
+  scrollToEnd(params?: ?{animated?: ?boolean}) {
+      if (this._listRef) {
+        this._listRef.scrollToEnd(params);
+      }
+    }
+
+
+scrollToIndex(params: {
+    animated?: ?boolean,
+    index: number,
+    viewOffset?: number,
+    viewPosition?: number,
+  }) {
+    if (this._listRef) {
+      this._listRef.scrollToIndex(params);
+    }
+  }
+
+  scrollToItem(params: {
+    animated?: ?boolean,
+    item: ItemT,
+    viewPosition?: number,
+  }) {
+    if (this._listRef) {
+      this._listRef.scrollToItem(params);
+    }
+  }
+
+  scrollToOffset(params: {animated?: ?boolean, offset: number}) {
+    if (this._listRef) {
+      this._listRef.scrollToOffset(params);
+    }
   }
 
   render() {
     return (
       <FlatList
+        ref={ref => this._listRef = ref}
         {...this.props}
         renderItem={ data => this._renderItem(data) }
         onViewableItemsChanged={this._onViewableItemsChanged}
